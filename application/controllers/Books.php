a78e6b64505954940147d4b9f0ec9b5012b73a93
hprasetyou <hprasetyou@gmail.com>
2017-01-02 22:30:54 +0700

    load book_list view to index

--- application/controllers/Books.php
+++ application/controllers/Books.php
@@ -3,11 +3,7 @@
 class Books extends CI_Controller{
 
   public function index(){
-    echo "<h1>Halo, aku adalah controller books</h1>";
-  }
-
-  public function foo(){
-    echo "<p>Halo, aku adalah contoh action lain dari controller books</p>";
+    $this->load->view('book_list');
   }
 
 }
---
+++ application/views/book_list.php
@@ -0,0 +1,36 @@
+
+<h1>Book List</h1>
+<p>
+
+</p>
+<table>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Type</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>The Count of Monte Cristo</td>
+            <td>Alexander Dumas</td>
+            <td>Novel</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Mein Kamf</td>
+            <td>Adolf Hitler</td>
+            <td>Biography</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Heike Monogatari</td>
+            <td>Eiji Yoshikawa</td>
+            <td>Novel</td>
+        </tr>
+    </tbody>
+
+</table>
