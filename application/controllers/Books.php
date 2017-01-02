2409926002614f24ae9ec08f359d0980e1664fd6
hprasetyou <hprasetyou@gmail.com>
2017-01-02 22:08:43 +0700

    add controller books

---
+++ application/controllers/Books.php
@@ -0,0 +1,13 @@
+<?php
+
+class Books extends CI_Controller{
+
+  public function index(){
+    echo "<h1>Halo, aku adalah controller books</h1>";
+  }
+
+  public function foo(){
+    echo "<p>Halo, aku adalah contoh action lain dari controller books</p>";
+  }
+
+}
